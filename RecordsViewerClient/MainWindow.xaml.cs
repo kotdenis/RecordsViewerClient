102f483ff60821efd06b0405d22a3f16be2c6508
kotdeniss <kotdeniss@yandex.ru>
2019-09-15 21:33:30 +0300

    старт проекта

--- RecordsViewerClient/MainWindow.xaml.cs
+++ RecordsViewerClient/MainWindow.xaml.cs
@@ -18,7 +18,7 @@ namespace RecordsViewerClient
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MahApps.Metro.Controls.MetroWindow
     {
         public MainWindow()
         {
